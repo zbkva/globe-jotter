13c63ad0f2623741f4e78e047ba7bcffc67383f3
Anastasiia Zubkova <stasiya.mail@gmail.com>
2023-07-10 15:24:08 -0400

    next scaffold

---
+++ map-diary/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <h1>Welcome, Fernando</h1>
+    </>
+  );
+};
+
+export default Dashboard;
