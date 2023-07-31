6a13de8350ec926e82f2f94ac6dcfcc2e893374d
Anastasiia Zubkova <stasiya.mail@gmail.com>
2023-07-31 13:24:56 -0400

    removed unused auth

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -4,8 +4,6 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { PassportModule } from '@nestjs/passport';
 import { DiaryEntriesModule } from './diary-entries/diary-entries.module';
 import { PicturesModule } from './pictures/pictures.module';
 
@@ -14,8 +12,6 @@ import { PicturesModule } from './pictures/pictures.module';
     ConfigModule.forRoot(),
     PrismaModule,
     UsersModule,
-    AuthModule,
-    PassportModule.register({ session: true }),
     DiaryEntriesModule,
     PicturesModule,
   ],
